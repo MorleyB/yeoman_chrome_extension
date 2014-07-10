22f93d2e144cbf5a0efbef19642592472ec2ac1a
Chloe Oppenheim <chloe@Chloes-MacBook-Air.local>
2014-07-10 15:54:17 -0500

    select box changes

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -14,20 +14,33 @@ switch (RAILS_ENV) {
     envUrl = "http://lvh.me:3000/auth/google/";
 }
 
+function populateSelectBox(){
+   var resp = {"streams": [ {name: "theName", id: "1"}, {name: "otherName", id: "2"}]};
+   var optionTemplate = "<option value=\"{streamID}\">{streamName}</option>";
+   for(var i = 0; i < resp.streams.length; i++){
+	   var obj = resp.streams[i];
+	   var currentOptionTemplate = optionTemplate;
+	   currentOptionTemplate = currentOptionTemplate.replace("{streamID}", obj.id);
+	   currentOptionTemplate = currentOptionTemplate.replace("{streamName}", obj.name);
+	   $("#selectedStream").append($(currentOptionTemplate));
+   }
+}  
 
 function GETStream() {
   var xhr = new XMLHttpRequest();
   xhr.open("GET", "#{envUrl}/streams", true);
   xhr.onreadystatechange = function() {
     if (xhr.readyState == 4) {
-      // var resp = JSON.parse(xhr.responseText);
+       // var resp = JSON.parse(xhr.responseText);
       // pass to list stream function
     } else {
       // alert('not hooked up yet')
-      var resp = {"streams": [ {"sweet":"cookies", "savory":"pizza"}]}
+      //resp.streams 
+      //get select box from pop.html with jquery and create options for select box using resp
     }
   }
   xhr.send();
+  populateSelectBox(); 
 }
 
 GETStream();
