d28edf70ee6eb3cc18c64a7ccfbe3014bdf2f70d
Tejas Manohar <me@tejasmanohar.com>
2014-07-11 09:57:57 -0500

    checkpoint

--- app/scripts/content.js
+++ app/scripts/content.js
@@ -3,6 +3,7 @@ var j = jQuery.noConflict();
   setInterval(
     function() {
       addToInbox();
+      addToEmail();
     },
     250
   );
@@ -10,13 +11,16 @@ var j = jQuery.noConflict();
 
 // Inbox View
 function addToInbox() {
-  if(j('td.apU.xY').length && j('.inbox-container').length === 0) {
+  if(j('div.J-J5-Ji.J-JN-M-I-JG').length && j('.inbox-container').length === 0) {
     j('<div class="inbox-container"><div class="inbox-icon1" data-tooltip="Add contact to stream"></div></div>').insertAfter('td.apU.xY > span:first-child');
   }
 }
 
 function addToEmail() {
-} 
+  if(j('h2.hP').length && j(":contains('Add Contact to Stream')").length === 0) {
+    j('<div id="" class="T-I J-J5-Ji ar7 nf T-I-ax7 L3" role="button" tabindex="0" aria-haspopup="true" aria-expanded="false" style="-webkit-user-select: none;"><span class="Ykrj7b">Add Contact to Stream</span></div>').insertAfter('div.iH > div > div:nth-last-child(2)');
+  }
+}
 
 function manageContact() {
 // Manage Contact
