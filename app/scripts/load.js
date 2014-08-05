a09fa4236dd437bff20baa6cd247b3af0c1dd4b7
Tejas Manohar <me@tejasmanohar.com>
2014-08-05 09:57:01 -0500

    new rawgit url

--- app/scripts/load.js
+++ app/scripts/load.js
@@ -9,6 +9,6 @@ window.onload = function () {
   document.getElementsByTagName('body')[0].appendChild(sm);
 
   var sm = document.createElement('script');
-  sm.src = "https://rawgit.com/tejas-manohar/gmail-integration/master/js/gm_inject.js";
+  sm.src = "https://rawgit.com/MorleyB/yeoman_chrome_extension/master/app/scripts/gm_inject.js";
   document.getElementsByTagName('body')[0].appendChild(sm);
 }
