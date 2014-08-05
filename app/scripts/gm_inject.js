067910c7990922b98b29c45a2cd4aa88c06683b8
Tejas Manohar <me@tejasmanohar.com>
2014-08-05 15:24:14 -0500

    get values for email thread

--- app/scripts/gm_inject.js
+++ app/scripts/gm_inject.js
@@ -1,12 +1,15 @@
 var runCode = function() {
-  $(document.body).append('<div id="modal"><p>TESTING</p></div>');
-    var modal = $('#modal').plainModal({duration: 500});
-  $(":contains('Add Contact to Stream')").click(function(){
-    modal.plainModal('open');
-  });
+  // TODO: MODAL
+    // $(document.body).append('<div id="modal"><p>TESTING</p></div>');
+    //   var modal = $('#modal').plainModal({duration: 500});
+    // $(":contains('Add Contact to Stream')").click(function(){
+    //   modal.plainModal('open');
+    // });
   setInterval(
     function() {
       addButton();
+      // TEMPORARY - JUST GETTING VALUES TO USE
+      getValues();
     },
     250
   );
@@ -28,7 +31,6 @@ var checkLoaded = function() {
       }
     };
 
-    // your code
     runCode();
 
   } else {
@@ -41,5 +43,20 @@ checkLoaded();
 function addButton() {
   if($('h2.hP').length && $(":contains('Add Contact to Stream')").length === 0) {
     $('<div id="" class="T-I J-J5-Ji ar7 nf T-I-ax7 L3" role="button" tabindex="0" aria-haspopup="true" aria-expanded="false" style="-webkit-user-select: none;"><span class="Ykrj7b">Add Contact to Stream</span></div>').insertAfter('div.iH > div > div:nth-last-child(2)');
+  }
+}
+
+function getValues() {
+  url = location.href;
+  if(url.length() >= 55 && url.length() <= 60) {
+    // WHICH THREAD
+    var id = Gmail().get.email_id();
+    
+    // EVERYTHING ABOUT THREAD
+    var data = Gmail().get.email_data(id);
+
+    // EMAIL ADDRESSES
+    var me = Gmail().get.user_email();
+    var everyone = data.threads.people_involved; 
   }
 }
